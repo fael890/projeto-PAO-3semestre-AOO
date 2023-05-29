0a3b0d6c276e06819c9af60e81fc98a6aca6972d
Rafael Marinho <rafael222.rc@gmail.com>
2023-05-29 13:39:17 -0300

    Adicionando crud do mercado

--- core/views.py
+++ core/views.py
@@ -9,7 +9,7 @@ def cadastrar_promocao(request):
     form = FormPromocao(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('url_obter_promocoes')
+        return redirect('url_listagem_promocoes')
     contexto = {'form': form, 'txt_title': 'cad_prom', 'txt_descricao': 'Cadastro de Promoção'}
     return render(request, 'core/cadastro.html', contexto)
 def listar_promocoes(request):
@@ -29,4 +29,31 @@ def atualizar_promocao(request, id):
 def excluir_promocao(request, id):
     obj = Promocao.objects.get(id=id)
     obj.delete()
-    return redirect('url_exclui_promocao')
+    return redirect('url_listagem_promocoes')
+
+def cadastrar_mercado(request):
+    form = FormMercado(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem_mercados')
+    contexto = {'form': form, 'txt_title': 'cad_merc', 'txt_descrição': 'Cadastro de mercado'}
+    return render(request, 'core/cadastro.html', contexto)
+
+def listar_mercados(request):
+    dados = Mercado.objects.all()
+    contexto = {'dados': dados}
+    return render(request, 'core/listagem_mercados.html', contexto)
+
+def atualizar_mercado(request, id):
+    obj = Mercado.objects.get(id=id)
+    form = FormMercado(request.POST or None, instance=obj)
+    if form.is_valid():
+        form.save()
+        return redirect('url_atualiza_mercado')
+    contexto = {'form': form, 'txt_title': 'Atualização de Mercado', 'txt_descricao': 'Atualização de Mercado', 'txt_button': 'Atualizar'}
+    return render(request, 'core/cadastro.html', contexto)
+
+def excluir_mercado(request, id):
+    obj = Mercado.objects.get(id=id)
+    obj.delete()
+    return redirect('url_listagem_mercados')
--- projeto_pao/urls.py
+++ projeto_pao/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path('', inicio, name='url_principal'),
     path('cadastro-promocao/', cadastrar_promocao, name='url_cadastro_promocao'),
     path('listagem-promocoes/', listar_promocoes, name='url_listagem_promocoes'),
-    path('atualiza-promocao/', atualizar_promocao, name='url_atualiza_promocao'),
-    path('excluir-promocao/', excluir_promocao, name='url_exclui_promocao'),
+    path('atualiza-promocao/<int:id>/', atualizar_promocao, name='url_atualiza_promocao'),
+    path('excluir-promocao/<int:id>/', excluir_promocao, name='url_exclui_promocao'),
+    path('cadastro-mercado/', cadastrar_mercado, name='url_cadastro_mercado'),
+    path('listagem-mercados/', listar_mercados, name='url_listagem_mercados'),
+    path('atualiza-mercado/<int:id>/', atualizar_mercado, name='url_atualiza_mercado'),
+    path('excluir-mercado/<int:id>/', excluir_mercado, name='url_exclui_mercado'),
 ]
