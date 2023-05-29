280e346501ee83e1004297656b4939c35d2e5c04
Rafael Marinho <rafael222.rc@gmail.com>
2023-05-28 22:32:38 -0300

    Adicionando crud da promoção

--- core/views.py
+++ core/views.py
@@ -1,4 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.forms import *
+from core.models import *
 
 def inicio(request):
     return render(request, 'core/index.html')
+
+def cadastrar_promocao(request):
+    form = FormPromocao(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('url_obter_promocoes')
+    contexto = {'form': form, 'txt_title': 'cad_prom', 'txt_descricao': 'Cadastro de Promoção'}
+    return render(request, 'core/cadastro.html', contexto)
+def listar_promocoes(request):
+    dados = Promocao.objects.all()
+    contexto = {'dados': dados}
+    return render(request, 'core/listagem_promocoes.html', contexto)
+
+def atualizar_promocao(request, id):
+    obj = Promocao.objects.get(id=id)
+    form = FormPromocao(request.POST or None, request.FILES or None, instance=obj)
+    if form.is_valid():
+        form.save()
+        return redirect('url_atualiza_promocao')
+    contexto = {'form': form, 'txt_title': 'Atualização de Promoção', 'txt_descricao': 'Atualização de Promoção', 'txt_button': 'Atualizar'}
+    return render(request, 'core/cadastro.html', contexto)
+
+def excluir_promocao(request, id):
+    obj = Promocao.objects.get(id=id)
+    obj.delete()
+    return redirect('url_exclui_promocao')
--- projeto_pao/urls.py
+++ projeto_pao/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import inicio
+from core.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', inicio, name='url_principal'),
+    path('cadastro-promocao/', cadastrar_promocao, name='url_cadastro_promocao'),
+    path('listagem-promocoes/', listar_promocoes, name='url_listagem_promocoes'),
+    path('atualiza-promocao/', atualizar_promocao, name='url_atualiza_promocao'),
+    path('excluir-promocao/', excluir_promocao, name='url_exclui_promocao'),
 ]
